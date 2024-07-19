48e3d93661488ad4109b6eb5ddaa52fd0aabd457
Nash Keyes <ndbkeyes@gmail.com>
2024-07-18 22:48:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-function load() {
-    var file = new XMLHttpRequest();
-    file.open("GET", "/assets/bio.txt", true);
-    file.onreadystatechange = function() {
-      if (file.readyState === 4) && if (file.status === 200) {  // Makes sure the document is ready to parse and is found
-          text = file.responseText;
-          document.getElementById("bio").innerHTML = text;
-      }
+var file = new XMLHttpRequest();
+file.open("GET", "/assets/bio.txt", true);
+file.onreadystatechange = function() {
+    if (file.readyState === 4) && if (file.status === 200) {  // Makes sure the document is ready to parse and is found
+        text = file.responseText;
+        document.getElementById("bio").innerHTML = text;
     }
 }
-
-window.onLoad = load();
