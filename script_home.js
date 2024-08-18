f359c8b0004aca2eb4fddad3de73cdfdd7b90c2f
Nash Keyes <ndbkeyes@gmail.com>
2024-08-17 22:03:17 -0400

    Update

--- script_home.js
+++
@@ -1,8 +0,0 @@
-var file = new XMLHttpRequest();
-file.open("GET", "/assets/bio.txt", true);
-file.onreadystatechange = function() {
-    if (file.readyState === 4) && if (file.status === 200) {  // Makes sure the document is ready to parse and is found
-        text = file.responseText;
-        document.getElementById("bio").innerHTML = text;
-    }
-}
